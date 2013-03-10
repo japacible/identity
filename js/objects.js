38e9091856d07ca9ffd2f47cebbd63f9b35a3906
J Apacible <japacible@gmail.com>
2013-03-10 00:41:23 -0800

    test objects, lazyload.js

--- js/objects.js
+++ js/objects.js
@@ -1,6 +1,6 @@
 var objects = [
   {
-    "image":"http://doodlingdogs.com/wp-content/uploads/2011/06/corgi.jpg",
+    "image":"img/sooyoung_one.jpeg",
     "name":"image one",
     "source": "source here",
     "link":"http://www.google.com",
@@ -9,7 +9,7 @@ var objects = [
     "comment":"insert comment here"
   },
   {
-    "image":"http://media-cache-ec2.pinterest.com/550x/ed/4f/e1/ed4fe147ef0c57bf6b21c979310322be.jpg",
+    "image":"img/leeminho_one.jpg",
     "name":"image two",
     "source": "source here",
     "link":"http://www.images.google.com",
@@ -18,12 +18,363 @@ var objects = [
     "comment":"insert comment here"
   },
   {
-    "image":"img/sooyoung.jpg",
+    "image":"img/leeminho_two.jpg",
     "name":"image three",
     "source": "source here",
     "link":"http://www.news.google.com",
     "tags":"celebrity",
     "origin":"kor",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_one.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_two.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_three.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_four.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_five.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_six.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_seven.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_eight.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_nine.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_ten.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_eleven.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+{
+    "image":"img/sooyoung_one.jpeg",
+    "name":"image one",
+    "source": "source here",
+    "link":"http://www.google.com",
+    "tags":"food",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/leeminho_one.jpg",
+    "name":"image two",
+    "source": "source here",
+    "link":"http://www.images.google.com",
+    "tags":"fashion",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/leeminho_two.jpg",
+    "name":"image three",
+    "source": "source here",
+    "link":"http://www.news.google.com",
+    "tags":"celebrity",
+    "origin":"kor",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_one.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_two.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_three.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_four.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_five.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_six.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_seven.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_eight.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_nine.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_ten.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_eleven.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+{
+    "image":"img/sooyoung_one.jpeg",
+    "name":"image one",
+    "source": "source here",
+    "link":"http://www.google.com",
+    "tags":"food",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/leeminho_one.jpg",
+    "name":"image two",
+    "source": "source here",
+    "link":"http://www.images.google.com",
+    "tags":"fashion",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/leeminho_two.jpg",
+    "name":"image three",
+    "source": "source here",
+    "link":"http://www.news.google.com",
+    "tags":"celebrity",
+    "origin":"kor",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_one.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_two.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_three.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_four.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_five.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_six.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_seven.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_eight.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_nine.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_ten.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/baxter_eleven.jpg",
+    "name":"baxter",
+    "source": "source here",
+    "link":"http://www.corgiland.com",
+    "tags":"corgi",
+    "origin":"usa",
     "comment":"insert comment here"
   }
 ];

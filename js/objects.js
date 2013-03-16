a2dc825d8886a2f715661a455c55b47a61ff5bcf
japacible <japacible@gmail.com>
2013-03-16 12:02:29 -0700

    update objects array

--- js/objects.js
+++ js/objects.js
@@ -1,34 +1,450 @@
 var objects = [
   {
-    "image":"img/sooyoung_one.jpeg",
-    "name":"image one",
-    "source": "source here",
-    "link":"http://www.google.com",
+    "image":"img/both/baxter.jpg",
+    "name":"Baxter",
+    "source": "Kawafune",
+    "link":"http://corgicorner.com",
     "origin":"asia usa",
     "comment":"insert comment here"
   },
   {
-    "image":"img/baxter_five.jpg",
-    "name":"baxter",
-    "source": "source here",
-    "link":"http://www.corgiland.com",
-    "origin":"usa",
+    "image":"img/both/chess.jpg",
+    "name":"Chess",
+    "source": "US Chess Federation",
+    "link":"http://www.uschess.org",
+    "origin":"asia usa",
     "comment":"insert comment here"
   },
   {
-    "image":"img/baxter_five.jpg",
-    "name":"baxter",
-    "source": "source here",
-    "link":"http://www.corgiland.com",
+    "image":"img/both/freddie-wong.jpg",
+    "name":"Freddie Wong",
+    "source": "YouTube Channel",
+    "link":"http://www.youtube.com/user/freddiew",
+    "origin":"asia usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/both/pad-see-ew.jpg",
+    "name":"Pad See Ew",
+    "source": "Thai Table",
+    "link":"http://www.thaitable.com/thai/recipe/pad-see-ew",
+    "origin":"asia usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/both/pad-thai.jpg",
+    "name":"image one",
+    "source": "Food Network",
+    "link":"http://www.foodnetwork.com/recipes/alton-brown/pad-thai-recipe/index.html",
+    "origin":"asia usa",
+    "comment":"insert comment here"
+  },
+  
+{    "image":"img/both/pho.jpg",
+    "name":"Pho",
+    "source": "Than Brothers",
+    "link":"http://www.thanbrothers.com/",
+    "origin":"asia usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/both/rubiks.jpg",
+    "name":"Rubik's Cube",
+    "source": "badmephisto",
+    "link":"http://www.badmephisto.com",
+    "origin":"asia usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/mix/arden-cho.jpg",
+    "name":"Arden Cho",
+    "source": "Official Site",
+    "link":"http://www.ardencho.com/",
+    "origin":"mix",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/mix/aziatix.jpg",
+    "name":"Aziatix",
+    "source": "Official Site",
+    "link":"http://www.aziatix.com/",
+    "origin":"mix",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/mix/david-choi.jpg",
+    "name":"David Choi",
+    "source": "Official Site",
+    "link":"http://davidchoimusic.com/",
+    "origin":"mix",
+    "comment":"insert comment here"
+  },  
+  {
+    "image":"img/mix/jason-chen.jpg",
+    "name":"Jason Chen",
+    "source": "Official Site",
+    "link":"http://jasondchen.com/",
+    "origin":"mix",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/mix/jimmy-wong.jpg",
+    "name":"Jimmy Wong",
+    "source": "YouTube Channel",
+    "link":"http://www.youtube.com/user/jimmy",
+    "origin":"mix",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/mix/ryan-higa.jpg",
+    "name":"Ryan Higa",
+    "source": "YouTube Channel",
+    "link":"http://www.youtube.com/user/nigahiga",
+    "origin":"mix",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/mix/wong-fu-productions.jpg",
+    "name":"Wong Fu Productions",
+    "source": "Official Site",
+    "link":"http://wongfuproductions.com/",
+    "origin":"mix",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/2ne1.jpg",
+    "name":"2NE1",
+    "source": "YouTube Channel",
+    "link":"http://www.youtube.com/user/2NE1",
     "origin":"asia",
     "comment":"insert comment here"
   },
   {
-    "image":"img/leeminho_two.jpg",
-    "name":"baxter",
-    "source": "source here",
-    "link":"http://www.corgiland.com",
-    "origin":"mix",
+    "image":"img/asian/bittermelon-stir-fry.jpg",
+    "name":"Bittermelon Stir Fry",
+    "source": "Serious Eats",
+    "link":"http://www.seriouseats.com/recipes/2010/07/seriously-asian-bitter-melon-stir-fry.html",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/chicken-feet.jpg",
+    "name":"Chicken Feet",
+    "source": "Spark Recipes",
+    "link":"http://recipes.sparkpeople.com/recipe-detail.asp?recipe=1859445",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/congee.jpg",
+    "name":"Congee",
+    "source": "All Recipes",
+    "link":"http://allrecipes.com/recipe/pork-and-century-egg-rice-congee/",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/daesung-nikon.jpg",
+    "name":"Daesung Nikon CF",
+    "source": "Nikon",
+    "link":"http://www.nikon.com",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/dan-tat.JPG",
+    "name":"Dan Tat",
+    "source": "Nee Hao",
+    "link":"http://www.neehao.co.uk/2011/11/chinese-egg-tarts-recipe-dan-tat/",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/durian.JPG",
+    "name":"Durian",
+    "source": "Year of the Durian",
+    "link":"http://www.yearofthedurian.com/p/durian-recipes.html",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/green-onion-pancake.jpg",
+    "name":"Green Onion Pancake",
+    "source": "Serious Eats",
+    "link":"http://www.seriouseats.com/recipes/2011/04/extra-flaky-scallion-pancakes-recipe.html",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/green-tea-boba.jpg",
+    "name":"Green Tea Boba",
+    "source": "Mee Sum Pastry",
+    "link":"http://www.yelp.com/biz/mee-sum-pastry-seattle-2",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/hello-kitty.jpg",
+    "name":"Hello Kitty",
+    "source": "Sanrio",
+    "link":"www.sanrio.com",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/iris-poster.jpg",
+    "name":"IRIS",
+    "source": "Drama Wiki",
+    "link":"http://wiki.d-addicts.com/IRIS",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/jang-geun-suk-babyandme-poster.jpg",
+    "name": "Baby and Me",
+    "source": "Asian Wiki",
+    "link":"http://asianwiki.com/Baby_and_Me",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/leche-flan.JPG",
+    "name":"Leche Flan",
+    "source": "Tagalog Lang",
+    "link":"http://tagaloglang.com/Filipino-Food/Desserts/leche-flan-recipe.html",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/lee-min-ho.jpg",
+    "name":"Lee Min Ho",
+    "source": "Asian Wiki",
+    "link":"http://asianwiki.com/Lee_Min-Ho",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/mapo-tofu.jpg",
+    "name":"Mapo Tofu",
+    "source": "Chinese Food",
+    "link":"http://chinesefood.about.com/od/tof1/r/mapotofu.htm",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/okonomiyaki.jpg",
+    "name":"Okonomiyaki",
+    "source": "Okonomiyaki World",
+    "link":"http://okonomiyakiworld.com/",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/park-shin-hye.jpg",
+    "name":"Park Shin Hye",
+    "source": "Asian Wiki",
+    "link":"http://asianwiki.com/Park_Shin-Hye",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/piano.jpg",
+    "name":"Piano",
+    "source": "Kennelly Keys Music",
+    "link":"http://www.kennellykeysmusic.com/",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/ping-pong.jpg",
+    "name":"Ping Pong",
+    "source": "Pong World: Rules",
+    "link":"http://www.pongworld.com/more/rules.php",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/sashimi.JPG",
+    "name":"Sashimi",
+    "source": "Fish & Seafood Cooking",
+    "link":"http://fishcooking.about.com/od/rawfish/r/tuna_sashimi.htm",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/secret-poster.jpg",
+    "name":"Secret",
+    "source": "Asian Wiki",
+    "link":"http://asianwiki.com/Secret_(2007-Taiwan)",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/shinee-ring-ding-dong.jpg",
+    "name":"SHINee",
+    "source": "YouTube Channel",
+    "link":"http://www.youtube.com/user/SHINee",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/starcraft2.jpg",
+    "name":"Starcraft II",
+    "source": "Blizzard Entertainment",
+    "link":"http://us.battle.net/sc2/",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/takoyaki.jpg",
+    "name":"Takoyaki",
+    "source": "Japan Centre",
+    "link":"http://www.japancentre.com/recipes/takoyaki",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/teresa-teng.jpg",
+    "name":"Teresa Teng",
+    "source": "Fan Page",
+    "link":"http://teresateng.org/",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/totoro.jpg",
+    "name":"My Neighbor Totoro",
+    "source": "IMDb",
+    "link":"http://www.imdb.com/title/tt0096283/",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/turnip-cake.jpg",
+    "name":"Turnip Cake",
+    "source": "Jeanette's Healthy Living",
+    "link":"http://jeanetteshealthyliving.com/2012/10/chinese-turnip-cake-recipe.html",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/udon-noodles.jpg",
+    "name":"Udon",
+    "source": "U:Don",
+    "link":"http://www.freshudon.com",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/wang-lee-hom.jpg",
+    "name":"Wang Lee Hom",
+    "source": "Official Site",
+    "link":"http://www.wangleehom.com",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/asian/xiao-long-bao.jpg",
+    "name":"Xiao Long Bao",
+    "source": "Din Tai Fung",
+    "link":"http://www.dintaifung.com.tw",
+    "origin":"asia",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/alex-goot.jpg",
+    "name":"Alex Goot",
+    "source": "YouTube Channel",
+    "link":"www.youtube.com/user/gootmusic",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/beyonce.jpg",
+    "name":"Beyonce",
+    "source": "Official Site",
+    "link":"http://www.beyonce.com",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/bobby-flay.jpg",
+    "name":"Bobby Flay",
+    "source": "Official Site",
+    "link":"http://www.bobbyflay.com",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/boyce-avenue.jpg",
+    "name":"Boyce Avenue",
+    "source": "Official Site",
+    "link":"http://www.boyceavenue.com/",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/california-sushi.jpg",
+    "name":"California Roll",
+    "source": "Food Network",
+    "link":"http://www.foodnetwork.com/recipes/alton-brown/california-roll-recipe/index.html",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/ellen-degeneres.jpg",
+    "name":"Ellen Degeneres",
+    "source": "Official Site",
+    "link":"http://www.ellentv.com",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/gordon-ramsay.jpg",
+    "name":"Gordon Ramsay",
+    "source": "Official Site",
+    "link":"http://www.gordonramsay.com",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/maroon-5.jpg",
+    "name":"Maroon 5",
+    "source": "Official Site",
+    "link":"http://www.maroon5.com",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/matrix-jp.jpg",
+    "name":"The Matrix",
+    "source": "IMDb",
+    "link":"http://www.imdb.com/title/tt0133093",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/mission-impossible.jpg",
+    "name":"Mission Impossible",
+    "source": "IMDb",
+    "link":"http://www.imdb.com/title/tt1229238",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/return-of-the-king.jpg",
+    "name":"The Lord of the Rings",
+    "source": "IMDb",
+    "link":"http://www.imdb.com/title/tt0167260",
+    "origin":"usa",
+    "comment":"insert comment here"
+  },
+  {
+    "image":"img/usa/taylor-swift.jpg",
+    "name":"Taylor Swift",
+    "source": "Official Site",
+    "link":"http://www.taylorswift.com",
+    "origin":"usa",
     "comment":"insert comment here"
   }
 ];
